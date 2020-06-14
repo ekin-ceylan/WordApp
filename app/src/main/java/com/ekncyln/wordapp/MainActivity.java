838db44022348e8689f92dc9a43e26663b5882b7
Ekin Ceylan <ekin@Ekin-MacBook-Pro.local>
2020-06-14 18:07:02 +0300

    first commit

---
+++ app/src/main/java/com/ekncyln/wordapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ekncyln.wordapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/ekncyln/wordapp/SetActivity.java
@@ -0,0 +1,13 @@
+package com.ekncyln.wordapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set);
+    }
+}
